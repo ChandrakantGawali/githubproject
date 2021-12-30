c4a55bd5f40fdcaebff7238f8aa0e14de9dec776
Chandrakant Gawali <chandrakantcjg@gmail.com>
2021-12-30 18:50:18 +0530

    first git repo

---
+++ Main.java
@@ -0,0 +1,16 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          @SuppressWarnings("resource")
+		Scanner sc =new Scanner(System.in);
+          String name =sc.nextLine();
+          System.out.println("my name is "+ name);
+          
+	}
+
+}
